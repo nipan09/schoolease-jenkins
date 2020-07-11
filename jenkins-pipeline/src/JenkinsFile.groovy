dc2cadec2dde20c077f1958a31062a56a88ab86e
nipan09 <pnishant46@gmail.com>
2020-07-12 01:41:16 +0530

    initial tests implemented

--- jenkins-pipeline/src/JenkinsFile.groovy
+++ jenkins-pipeline/src/JenkinsFile.groovy
@@ -1,3 +1,42 @@
-
-class JenkinsFile {
+pipeline{
+	agent any
+	stages
+	{   
+		stage('Build')
+		{
+			steps
+			{
+				echo "Building the project.."
+				sh'''
+                    source bin/activate
+					pip3 install -r requirements.txt
+				    deactivate
+                '''
+			}
+		}
+		stage('Collect Static files')
+		{
+			steps
+			{
+				echo "Collecting static files.."
+				sh'''
+					source bin/activate
+					python3 manage.py collectstatic --noinput
+				    deactivate
+				'''
+			}
+		}
+		stage('Run Unit/ Integration Tests')
+		{
+			steps
+			{
+				echo "Running Unit/Integration Tests.."
+				sh'''
+					source bin/activate
+					python3 manage.py test
+					deactivate
+				'''
+			}
+		}
+	}
 }
